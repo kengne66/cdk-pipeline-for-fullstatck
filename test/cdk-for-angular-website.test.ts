a205db94d3ee004c2fba87171deb5cfd5d29d8ad
Pierre <pierre_kengne@yahoo.com>
2023-05-21 10:25:04 -0400

    first commit

---
+++ test/cdk-for-angular-website.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkForAngularWebsite from '../lib/cdk-for-angular-website-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-for-angular-website-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkForAngularWebsite.CdkForAngularWebsiteStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
