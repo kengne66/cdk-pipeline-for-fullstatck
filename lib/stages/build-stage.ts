1913c7b3de24785ac30b40cbc6498e3271fd60a4
Pierre <pierre_kengne@yahoo.com>
2023-05-28 16:17:02 -0400

    commit

--- lib/stages/build-stage.ts
+++ lib/stages/build-stage.ts
@@ -1,11 +1,11 @@
 
-import { Stage, StageProps, CfnOutput } from "aws-cdk-lib";
+import { Stage, StageProps, CfnOutput, Stack } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 
 export class buildStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
-
+    new Stack(this, "buildStack", props)
   }
 }
