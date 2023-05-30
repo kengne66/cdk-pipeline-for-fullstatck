b6567d8801eb381c8360aa12c4f8d7a40189ec67
Pierre <pierre_kengne@yahoo.com>
2023-05-30 07:50:49 -0400

    add lambda

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -24,7 +24,7 @@ export class LambdaStack extends Stack {
     const Lambda = new Function(this, "serviceLambda", {
       runtime: Runtime.NODEJS_14_X,
       handler: "scr/lambda.handler",
-      code: Code.fromAsset('../lambda-code/scr/lambda'),
+      code: Code.fromAsset('../lambda-code/src/lambda'),
       functionName: `serviceLambda${props.stageName}`,
       description: `Generated on ${new Date().toISOString}`
     });
